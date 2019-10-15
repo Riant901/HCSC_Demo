364874e92a5c9687ac4ddb3b072e39524ea5f095
Riant KR <riant.raman@infosys.com>
2019-10-15 13:48:35 +0530

    Config Upated

--- Demo/Jenkinsfile_demo.groovy
+++ Demo/Jenkinsfile_demo.groovy
@@ -13,40 +13,41 @@ node ('master') {
     stage('Build-Package and Upload') {
         sh '''
         pwd
-        ws="/var/lib/jenkins/workspace/Demo"
-        rm -rf output
-        mkdir output
-        cd output
-        cp -rf $ws/multi-module/webapp/target/*.war .
+        ws="/var/lib/jenkins/workspace/HCSC_Dev_Build_Deploy"
+        rm -rf hcsc_output
+        mkdir hcsc_output
+        cd hcsc_output
+        cp -rf $ws/target/*.war .
         cp -rf $ws/Configs/*.config .
         cd $ws
-        zip -r output.zip output
+        zip -r hcsc_output.zip hcsc_output
         echo "Upload the Build Artifacts"
-        curl -u admin:Db7Xu8Sd7Bd6Gr -X PUT "https://jfroguser.jfrog.io/jfroguser/hcsc_dev/output.zip" -T output.zip
+        curl -u admin:Db7Xu8Sd7Bd6Gr -X PUT "https://jfroguser.jfrog.io/jfroguser/hcsc_dev/hcsc_output.zip" -T hcsc_output.zip
         echo "Upload the Build Artifact Completed"
         '''
     }
     stage('Dev Deployment') {
         sh '''
         pwd
-        ws="/var/lib/jenkins/workspace/Demo/Deployment"
+        ws="/var/lib/jenkins/workspace/HCSC_Dev_Build_Deploy/Deployment"
+        mkdir -p /var/lib/jenkins/workspace/HCSC_Dev_Build_Deploy/Deployment/stage/dev
         cd $ws/stage
         echo "Download the artifacts for Deployment"
         wget --user=admin --password=Db7Xu8Sd7Bd6Gr -r --no-parent -nH --cut-dirs=2 "https://jfroguser.jfrog.io/jfroguser/hcsc_dev/"
         echo "Download Completed"
         echo "Connect to Deployment Servers"
         phase="dev"
-        unzip output.zip
+        unzip hcsc_output.zip
         echo "Application Instance Stopped"
-        cp -rf $ws/stage/output/*.war $ws/dev/webapps/
-        cp -rf $ws/stage/output/$phase.config $ws/dev/configs/
+        cp -rf $ws/stage/hcsc_output/*.war $ws/dev/webapps/
+        cp -rf $ws/stage/hcsc_output/$phase.config $ws/dev/configs/
         echo "Application Instance Started" 
         echo "Deployment Completed"
         '''
     }
     stage('Liquibase DB Deployment') {
         sh '''
-        ws="/var/lib/jenkins/workspace/Demo"
+        ws="/var/lib/jenkins/workspace/HCSC_Dev_Build_Deploy"
         cd $ws/liquibase
         ./liquibase  --driver=org.postgresql.Driver --classpath=postgresql-42.2.8.jar --url="jdbc:postgresql://localhost:5432/capDB" --changeLogFile=db.changelog-1.0.xml --username=shivam --password=devops@123 generateChangeLog
         '''
@@ -68,7 +69,7 @@ node ('master') {
     stage('Promote Artifacts for QA Deployment') {
        sh '''
        echo "Promoting the Artifacts to QA started"
-            curl -X POST -u admin:Db7Xu8Sd7Bd6Gr "https://jfroguser.jfrog.io/jfroguser/api/copy/hcsc_dev/output.zip?to=/hcsc_qa/output.zip"
+            curl -X POST -u admin:Db7Xu8Sd7Bd6Gr "https://jfroguser.jfrog.io/jfroguser/api/copy/hcsc_dev/hcsc_output.zip?to=/hcsc_qa/hcsc_output.zip"
        echo "Promoting the Artifcats to QA Completed"
             '''
         }
@@ -77,7 +78,7 @@ node ('master') {
             " Helvetica, sans-serif;}th, td {text-align: left; padding: 8px;font-size: 11px;} tr:nth-child(even)" +
             "{background-color: #f2f2f2} th { background-color: #4CAF50;color: white;}th, td { border: 1px solid #ddd;}h3," +
             "th{ text-transform: uppercase;}</style></head></br><table><tr><th colspan='2'>HCSC Deployment</th></tr><tr>" +
-            " <td><strong>PROJECT NAME</strong></td><td>${JOB_NAME}</td></tr><tr><td><strong>BUILD LABEL</strong></td><td>output.zip" +
+            " <td><strong>PROJECT NAME</strong></td><td>${JOB_NAME}</td></tr><tr><td><strong>BUILD LABEL</strong></td><td>hcsc_output.zip" +
             "</td></tr><tr><td><strong>SERVERS DEPLOYED</strong></td><td>Deployed on Dev</td></tr><tr><td><strong>STATUS</strong>" +
             "</td><td>${currentBuild.result}" +
             "</td></tr><tr><td><strong>Started By</strong></td><td>Admin</td></tr></table>"
